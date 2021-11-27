b5578a092fdcb24b19cb5e598de3d3a2614f0b38
zhangzhigang <937795833@qq.com>
2021-11-28 04:17:26 +0800

    JZ-22 判断平衡二叉树2

--- src/Tree/JZ28.java
+++ src/Tree/JZ28.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import java.util.LinkedList;
+
 /**
  * 对称二叉树 剑指22题
  */
@@ -24,7 +26,7 @@ public class JZ28 {
     }
 
 
-    //以下为提交代码 F1
+    // F1：递归+DFS
     class Solution {
         public boolean isSymmetric(TreeNode root) {
             return root == null ? true : help(root.left, root.right);
@@ -40,5 +42,38 @@ public class JZ28 {
             return help(left.left, right.right) && help(left.right, right.left);
         }
     }
+
+    // F2 队列方式实现
+    class Solution2 {
+        public boolean isSymmetric(TreeNode root) {
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            queue.add(root.left);
+            queue.add(root.right);
+            while (queue.size() > 0) {
+                TreeNode left = queue.removeFirst();
+                TreeNode right = queue.removeFirst();
+                // 则只有均空时为真，不可return，继续执行
+                if (left == null && right == null) {
+                    continue;
+                }
+                // 剩余为其中一个为空，不对称，直接返回
+                if (left == null || right == null) {
+                    return false;
+                }
+                if (left.val != right.val) {
+                    return false;
+                }
+                // 左左&&右右
+                queue.add(left.left);
+                queue.add(right.right);
+                // 左右&&右左
+                queue.add(left.right);
+                queue.add(right.left);
+            }
+            return true;
+        }
+    }
+
+
 }
 
--- src/Tree/JZ55.java
+++ src/Tree/JZ55.java
@@ -20,7 +20,8 @@ public class JZ55 {
         TreeNode() {
         }
     }
-    // 以下为提交的代码
+
+    // F1 自顶向上的方式
     class Solution {
         public boolean isBalanced(TreeNode root) {
             return helper(root) != -1;
@@ -36,6 +37,24 @@ public class JZ55 {
                 return -1;
             }
             return Math.max(left, right) + 1;
+        }
+    }
+
+    // F2 自顶向下的方式
+    class Solution2 {
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) return true;
+            else {
+                return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+            }
+
+        }
+
+        public int height(TreeNode root) {
+            if (root == null) return 0;
+            else {
+                return Math.max(height(root.left), height(root.right)) + 1;
+            }
         }
     }
 
