0ac835d02ccb410da110d92f15a3e50b447b963a
zhangzhigang <937795833@qq.com>
2021-12-02 23:56:17 +0800

    层次遍历

--- src/Tree/LC102.java
+++ src/Tree/LC102.java
@@ -31,14 +31,14 @@ public class LC102 {
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
             List<List<Integer>> ret = new ArrayList<List<Integer>>();
-            if (root == null) return null;
+            if (root == null) return ret;
 
             Queue<TreeNode> queue = new LinkedList<TreeNode>();
             queue.offer(root);
-            while (queue.isEmpty()) {
+            while (!queue.isEmpty()) {
                 List<Integer> level = new ArrayList<>();
                 int currentLevelSize = queue.size();
-                for (int i = 0; i <= currentLevelSize; ++i) {
+                for (int i = 1; i <= currentLevelSize; ++i) {
                     TreeNode node = queue.poll();
                     level.add(node.val);
                     if (node.left != null) {
@@ -50,7 +50,7 @@ public class LC102 {
                 }
                 ret.add(level);
             }
-        return ret;
+            return ret;
         }
 
     }
