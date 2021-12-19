f06167f547daad498c7386332dde55b182176b47
zhangzhigang <937795833@qq.com>
2021-12-19 23:44:23 +0800

    层序遍历LC102、LC105、LC107

--- src/DP/LC5最长回文字符串.java
+++ src/DP/LC5最长回文字符串.java
@@ -1,39 +1,39 @@
-package DP;
-
-
-/**
- * 最长回文字符串
- * 1、原问题与子问题：子字符串也是回文
- * 2、动态规划的状态：
- * 3、边界条件：1、2
- * 4、转义方程：P(i,j) = P(i+1,j-1) & Si = Sj
- */
-public class LC5最长回文字符串 {
-    class Solution1 {
-        // 暴力法
-        public String longestPalindrome(String s) {
-            int len = s.length();
-            if (len < 2) return s;
-
-            char[] charArray = s.toCharArray();
-            int begin = 0;
-            int maxLine = 1;
-
-            for (int i = 0; i < len - 1; i++)
-                for (int j = i + 1; j < len; j++) {
-                    if (j - i + 1 > maxLine&&)
-                    {
-
-                    }
-                }
-
-
-        }
-
-        public boolean checkString(int i, int j,   ){
-
-        }
-    }
-
-
-}
+//package DP;
+//
+//
+///**
+// * 最长回文字符串
+// * 1、原问题与子问题：子字符串也是回文
+// * 2、动态规划的状态：
+// * 3、边界条件：1、2
+// * 4、转义方程：P(i,j) = P(i+1,j-1) & Si = Sj
+// */
+//public class LC5最长回文字符串 {
+//    class Solution1 {
+//        // 暴力法
+//        public String longestPalindrome(String s) {
+//            int len = s.length();
+//            if (len < 2) return s;
+//
+//            char[] charArray = s.toCharArray();
+//            int begin = 0;
+//            int maxLine = 1;
+//
+//            for (int i = 0; i < len - 1; i++)
+//                for (int j = i + 1; j < len; j++) {
+////                    if (j - i + 1 > maxLine&&)
+//                    {
+//
+//                    }
+//                }
+//
+//
+////        }
+////
+////        public boolean checkString(int i, int j,   ){
+////
+////        }
+//    }
+//
+//
+//}
---
+++ src/LCTEST/lc333.java
@@ -0,0 +1,20 @@
+package LCTEST;
+
+public class lc333 {
+    class Solution {
+        public String firstPalindrome(String[] words) {
+
+//        }
+
+//        public boolean check(String word) {
+//            char[] c = word.toCharArray();
+//            int len  = c.length;
+//            if (len < 2) return true;
+//            for (int i = 0; i < len;i++){
+//                for(int j = i+1; j<len)
+//            }
+
+
+        }
+    }
+}
